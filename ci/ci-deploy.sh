6a99a47eff4381a4e528d40c53cdb3b3c82e71b4
Chris Busch <cbusch@pivotal.io>
2016-05-17 08:33:48 -0500

    updates

--- ci/ci-deploy.sh
+++ ci/ci-deploy.sh
@@ -3,6 +3,6 @@
 set -e -x
 
 fly -t ccb login -c http://192.168.100.4:8080
-fly -t ccb set-pipeline --pipeline pcf-ers-demo --config pipeline.yml --load-vars-from .cf-env.yml
+fly -t ccb set-pipeline --pipeline pcf-ers-demo --config pipeline-new.yml --load-vars-from ~/.concourse/ci-props.yml
 fly -t ccb unpause-pipeline --pipeline pcf-ers-demo
 
--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -2,9 +2,75 @@
 
 set -e -x
 
-pushd source-code
-  ./mvnw clean package
-popd
+#pushd source-code
+#  ./mvnw clean package
+#popd
 
-cp source-code/target/pcf-ers-demo1-0.0.1-SNAPSHOT.jar build-output/.
+#cp source-code/target/pcf-ers-demo-0.0.1.jar build-output/.
+
+# FROM PCF-DEMO REPO ----
+
+# args
+inputDir=  outputDir=  versionFile=  artifactId=  packaging=
+
+while [ $# -gt 0 ]; do
+  case $1 in
+    -i | --input-dir )
+      inputDir=$2
+      shift
+      ;;
+    -o | --output-dir )
+      outputDir=$2
+      shift
+      ;;
+    -v | --version-file )
+      versionFile=$2
+      shift
+      ;;
+    -a | --artifactId )
+      artifactId=$2
+      shift
+      ;;
+    -p | --packaging )
+      packaging=$2
+      shift
+      ;;
+    * )
+      echo "Unrecognized option: $1" 1>&2
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+error_and_exit() {
+  echo $1 >&2
+  exit 1
+}
+
+if [ ! -d "$inputDir" ]; then
+  error_and_exit "missing input directory: $inputDir"
+fi
+if [ ! -d "$outputDir" ]; then
+  error_and_exit "missing output directory: $outputDir"
+fi
+if [ ! -f "$versionFile" ]; then
+  error_and_exit "missing version file: $versionFile"
+fi
+if [ -z "$artifactId" ]; then
+  error_and_exit "missing artifactId!"
+fi
+if [ -z "$packaging" ]; then
+  error_and_exit "missing packaging!"
+fi
+
+version=`cat $versionFile`
+artifactName="${artifactId}-${version}.${packaging}"
+
+cd $inputDir
+./mvnw clean package -Pci -DversionNumber=$version
+
+# Copy jar file to concourse output folder
+cd ..
+cp $inputDir/target/$artifactName $outputDir/$artifactName
 
