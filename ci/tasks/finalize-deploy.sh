8c304fc0a27f0e56843b9954b99b9ec83cfd6a7d
Chris Busch <cbusch@pivotal.io>
2016-05-17 22:11:09 -0500

    finalize stuff

--- ci/tasks/finalize-deploy.sh
+++ ci/tasks/finalize-deploy.sh
@@ -4,20 +4,20 @@ set -e -x
 
 echo "Finalizing production deployment..."
 
-echo $API_ENDPOINT
-echo $USER
-echo $PW
-echo $ORG
-echo $SPACE
+echo $CF_API_ENDPOINT
+echo $CF_USER
+echo $CF_PWD
+echo $CF_ORG
+echo $CF_SPACE
 echo $CF_MANIFEST_HOST
 
 # get latest CF CLI
 curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
 ./cf --version
 
-./cf login -a ${API_ENDPOINT} -u ${USER} -p ${PW} -o ${ORG} -s ${SPACE} --skip-ssl-validation
+./cf login -a ${CF_API_ENDPOINT} -u ${CF_USER} -p ${CF_PWD} -o ${CF_ORG} -s ${CF_SPACE} --skip-ssl-validation
 
-./cf apps | grep ${CF_MANIFEST_HOST} | cut -d" " -f1 > current-apps.txt
+./cf apps | grep ${CF_HOST} | cut -d" " -f1 > current-apps.txt
 
 cat current-apps.txt
 
